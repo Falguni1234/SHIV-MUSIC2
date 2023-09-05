acd7d6aa55cda86be5e57a1752b81d918566f245
MISHSINGH19 <141798577+MISHSINGH19@users.noreply.github.com>
2023-09-05 09:37:55 +0530

    Update play.py

--- VipX/plugins/play.py
+++ VipX/plugins/play.py
@@ -34,7 +34,6 @@ PLAY_COMMAND = get_command("PLAY_COMMAND")
 @app.on_message(
     filters.command(PLAY_COMMAND)
     & filters.group
-    & filters.sender_chat
     & ~filters.edited
     & ~BANNED_USERS
 )
